26c34fa922d757f5700f992cfe2095074e0ee7fc
Chandra Pavan Prathapaneni <prathapanenichandrapavan@gmail.com>
2024-12-04 18:32:51 -0600

    Update config.js

--- js/config.js
+++ js/config.js
@@ -2,7 +2,7 @@ window._config = {
     cognito: {
         userPoolId: 'us-east-1_iNfwSDLz7', // e.g. us-east-2_uXboG5pAb
         userPoolClientId: '3rnqislc99d4ptahb7ri0egrnb', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
